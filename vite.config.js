5d0ca1fa0d1405a15daa31b367f589164e4e7a19
实验 <2557578722@qq.com>
2026-04-26 14:11:29 +0800

    在线上线

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/online/',
+  base: '/onlineproject/',
   plugins: [
     vue(),
     vueDevTools(),
